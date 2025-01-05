df899012b9e8bf21320956cd7896756acf516a04
김동현 <69367698+indextrown@users.noreply.github.com>
2025-01-06 01:33:04 +0900

    readme upload

--- CustomScrollAnimationCalendar/ContentView.swift
+++ CustomScrollAnimationCalendar/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader {
+            let safearea = $0.safeAreaInsets
+            
+            HomeView(safearea: safearea)
+                .ignoresSafeArea()
         }
-        .padding()
     }
 }
 
